2ca39d5a24b339b29b658c8be8c520d0ebf61291
Tomasz Osuch <tomasz.osuch@gmail.com>
2021-11-11 21:52:37 +0100

    Modyfikacja build.gradle o JDBC oraz dodanie konfiguracji dla JDBC

---
+++ src/main/java/config/JDBCConnectionConfig.java
@@ -0,0 +1,8 @@
+package config;
+
+public class JDBCConnectionConfig {
+
+    public static final String URL = "jdbc:oracle:thin:localhost:1521:xe";
+    public static final String user = "admin";
+    public static final String password = "admin";
+}
