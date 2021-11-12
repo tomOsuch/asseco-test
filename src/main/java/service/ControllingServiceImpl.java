d7f426f23788f3351e9fdf434469e0dd0d7a338c
Tomasz Osuch <tomasz.osuch@gmail.com>
2021-11-12 11:42:25 +0100

    Dodanie Wymagania 1 dla ksiegowosci oraz Wymagania 2 dla kontrolingu

---
+++ src/main/java/service/ControllingServiceImpl.java
@@ -0,0 +1,27 @@
+package service;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ControllingServiceImpl implements DbRequestService {
+
+    private static final String QUERY = "SELECT DISTINCT Departments.department_name " +
+            "FROM Job_history " +
+            "INNER JOIN Departments ON Job_history.department_id = Departments.department_id " +
+            "INNER JOIN Employees ON Job_history.employee_id = Employees.employee_id " +
+            "INNER JOIN Jobs ON Job_history.job_id = Jobs.job_id " +
+            "WHERE (Employees.last_name = 'Kowalski' OR Employees.last_name = 'Nowak') " +
+            "AND Employees.salary < Jobs.max_salary - (Jobs.max_salary * 25 / 100) " +
+            "ORDER BY Departments.department_name ASC;";
+
+    @Override
+    public void getDataRequest(Statement statement) throws SQLException {
+
+        ResultSet resultSet = statement.executeQuery(QUERY);
+        while (resultSet.next()) {
+            String departmentName = resultSet.getString("department_name");
+            System.out.println(departmentName);
+        }
+    }
+}
